e5a09a8008e72de7a02dda05e5860e356c063161
johanneshilmer <johanneshilmerson94@gmail.com>
2024-06-09 19:57:27 +0200

    asd

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
-
 const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+})
 
 
-//This is the server
-
-
-app.listen(8080, () => {
-  console.log("Server is running on port: 8080");
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
