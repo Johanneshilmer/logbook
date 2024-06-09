0b122dc7131a707dd5bd713ad41d049335647edc
johanneshilmer <johanneshilmerson94@gmail.com>
2024-06-09 20:57:27 +0200

    update

---
+++ client/src/components/Forms.jsx
@@ -0,0 +1,137 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import { FloatingLabel } from 'react-bootstrap';
+
+// Top-Bot-Setup
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+
+//Hooks
+import React, { useState } from 'react';
+
+export default function Forms({ dataForms, setDataForms }) {
+  
+  // Button TOP/BOT/SETUP
+  const [radioValue, setRadioValue] = useState('1');
+  const radios = [
+    { name: 'TOP', value: 'TOP' },
+    { name: 'BOTTOM', value: 'BOTTOM' },
+    { name: 'SETUP', value: 'SETUP' },
+  ];
+
+
+  let newDate = new Date()
+  let date = newDate.getDate();
+  let month = newDate.getMonth() + 1;
+  let year = newDate.getFullYear();
+
+  const showTime = newDate.getHours() 
+        + ':' + newDate.getMinutes() 
+        + ":" + newDate.getSeconds();
+
+
+  const [dataForm, setForm] = useState({
+    name: "",
+    workOrder: "",
+    program: "",
+    radios: "",
+    solderTest: false,
+    comment: "",
+    date: `${year}/${month<10?`0${month}`:`${month}`}/${date}`,
+    time: showTime,
+  });
+  // e == "event"
+  const handleChange = e => {
+    const { name, value, type, checked } = e.target;
+
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+
+    if (type === "radio") {
+      setRadioValue(value);
+    }
+  };
+  
+  const handleSubmit = e => {
+    e.preventDefault()
+    setDataForms([dataForm, ...dataForms ])
+  }
+
+
+  return (
+    <Form onSubmit={handleSubmit} method="POST">
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formId">
+          <FloatingLabel label="Enter Your ID">
+            <Form.Control type="text" placeholder="Your ID" value={dataForm.name} name="name" onChange={handleChange} />
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formWordOrder">
+          <FloatingLabel label="Work Order">
+            <Form.Control type="text" placeholder="Work Order" value={dataForm.workOrder} name="workOrder" onChange={handleChange} />
+          </FloatingLabel>
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formProgram">
+        <FloatingLabel label="Seplace Program">
+          <Form.Control placeholder="Siplace Program" name="program" value={dataForm.program} onChange={handleChange} />
+        </FloatingLabel>
+      </Form.Group>
+
+      {/* Button  */}
+      <Row>
+        <Col>
+          <ButtonGroup className='mt-1 mb-4'>
+            {radios.map((radio, idx) => (
+              <ToggleButton
+                key={radio.value}
+                id={`radio-${idx}`}
+                type="radio"
+                variant={idx % 2 ? 'outline-success' : 'outline-danger'}
+                name="radios"
+                value={radio.value}
+                checked={dataForm.radios === radio.value}
+                onChange={handleChange}
+              >
+                {radio.name}
+              </ToggleButton>
+            ))}
+          </ButtonGroup>
+        </Col>
+        <Col>
+          <Form.Check className='mt-2' type="switch" checked={dataForm.solderTest} id="custom-switch" label="Solderability Test" name="solderTest" onChange={handleChange} />
+        </Col>
+      </Row>
+      
+
+    {/* Comment */}
+      <Form.Group className='mb-3' controlId="floatingTextarea">
+        <FloatingLabel label="Comments">
+          <Form.Control as="textarea" placeholder="Comments" style={{ height: '80px' }} value={dataForm.comment} name="comment" onChange={handleChange} />
+        </FloatingLabel>
+      </Form.Group>
+
+      <Form.Group className='d-flex flex-row-reverse'>
+
+        <Button variant="success" type="submit">
+          START
+        </Button>
+
+        <Button variant="danger" type="submit">
+          STOP
+        </Button>
+
+        <Button variant="secondary" type="submit">
+          PAUSE
+        </Button>
+
+      </Form.Group>
+    </Form>
+  );
+}
---
+++ client/src/components/Tables.jsx
@@ -0,0 +1,69 @@
+import Table from 'react-bootstrap/Table';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default function Tables({ dataForms, dataList }) {
+
+  const deleteHandler = id => {
+    const newList = dataList.filter(item => {
+      return item.id !== id;
+    });
+
+    dataList(newList);
+  };
+
+  const updateHandler = () => {
+    //update, will fix this to morrow.
+  }
+
+  const updateDataListState = () => {
+
+  }
+
+  const updateAndReset = (input, e) => {
+    e.preventDefault()
+  }
+
+
+  const tableRows = dataForms.map((items) => {
+    return (
+      <tr key={items.id}>
+        <td>{items.date}</td>
+        <td>{items.time}</td>
+        <td>{items.workOrder}</td>
+        <td>{items.program}</td>
+        <td>{items.radios}</td>
+        <td>Worktime</td>
+        <td>{items.solderTest?"Y":"N"}</td>
+        <td>{items.name}</td>
+        <td>{items.comment}</td>
+        <td>
+          <Button className='editbtn' variant="secondary" onClick={updateHandler}>Edit</Button>
+          <Button className='deletebtn' variant="danger" onClick={() => deleteHandler(dataList.id)}>Delete</Button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <Table striped bordered hover responsive="xl">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Start Time</th>
+          <th>Order</th>
+          <th>Program</th>
+          <th>TOP/BOT/SETUP</th>
+          <th>Work Time</th>
+          <th>ST</th>
+          <th>ID</th>
+          <th>Comment</th>
+          <th>Change</th>
+        </tr>
+      </thead>
+      <tbody>
+          {tableRows}
+      </tbody>
+    </Table>
+  );
+}
